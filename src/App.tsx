63e0070ae9868ef81f636e102a29e93f8fd799db
sennett <anthony@oazoapps.com>
2022-05-24 15:02:39 +0100

    tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 import { OpenBorrowVault } from './flow/OpenBorrowVault'
 import { appContext } from './flow/hooks/useAppContext'
--- src/flow/hooks/useAppContext.tsx
+++ src/flow/hooks/useAppContext.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { AppContext, setupAppContext } from '../appContext'
 
 export const appContext = React.createContext<AppContext>(setupAppContext())
